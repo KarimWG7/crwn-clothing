f98fb0388c48a7c2f7843205389b4eb4673b379d
KarimWG7 <karimghanem871@gmail.com>
2023-07-30 18:54:30 +0200

    fininshing getting the data from the firestore db and making the categories preview

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,23 @@
-import React, { useContext } from "react";
-import { productsContext } from "../../contexts/product.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useContext } from "react";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 import "./shop.styles.scss";
 
-function Shop() {
-  const { products } = useContext(productsContext);
+const Shop = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map((product) => {
-        return <ProductCard key={product.id} product={product} />;
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
       })}
     </div>
   );
-}
+};
 
 export default Shop;
